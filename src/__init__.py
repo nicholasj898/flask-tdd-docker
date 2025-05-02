691221f200e4bcfe84069a27ed18dbe9e8f4d067
Nicholas Jones <nicholasj898@gmail.com>
2025-05-02 14:11:03 -0700

    Docker is now Setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 # src/__init__.py
 
 
+import os  # new
+
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 
@@ -11,7 +13,8 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('src.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)      # new
 
 
 class Ping(Resource):
@@ -20,6 +23,5 @@ class Ping(Resource):
             'status': 'success',
             'message': 'pong!'
         }
-
 
 api.add_resource(Ping, '/ping')

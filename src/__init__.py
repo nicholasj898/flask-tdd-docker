c7f0b977f09d9af9bab907a7318911e5240460ba
Nicholas Jones <nicholasj898@gmail.com>
2025-05-02 14:48:40 -0700

    SQL setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,7 @@
-# src/__init__.py
-
-
-import os  # new
-
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy  # new
 
 
 # instantiate the app
@@ -13,8 +10,24 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app_settings = os.getenv('APP_SETTINGS')  # new
-app.config.from_object(app_settings)      # new
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)  # new
+
+
+# model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class Ping(Resource):
@@ -23,5 +36,6 @@ class Ping(Resource):
             'status': 'success',
             'message': 'pong!'
         }
+
 
 api.add_resource(Ping, '/ping')

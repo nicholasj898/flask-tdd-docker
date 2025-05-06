ea7d4b4ac116fa750aefad0a5098af022ac97d4d
Nicholas Jones <nicholasj898@gmail.com>
2025-05-05 19:30:15 -0700

    import app

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
+# manage.py
+
+
+import sys
+
 from flask.cli import FlaskGroup
 
-from src import app, db  # new
+from src import create_app, db   # new
+from src.api.models import User  # new
 
 
-cli = FlaskGroup(app)
-
-
-# new
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)  # new
 @cli.command('recreate_db')
 def recreate_db():
     db.drop_all()
